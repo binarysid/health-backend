c31101d8c6bf87ae47e1bb85c89015502e3b1eeb
Linkon Sid <linkon@sheba.xyz>
2021-05-02 02:07:35 +0600

    push payload data type modified from int to string

--- HealthBackendProject/Service/PushNotification.py
+++ HealthBackendProject/Service/PushNotification.py
@@ -1,5 +1,4 @@
 from HealthBackendProject.Service import Firebase
-# from Hospital.models.DoctorAppointmentData import DoctorAppointmentData
 
 def initialize():
     Firebase.initApp()
@@ -8,8 +7,10 @@ def send_appntment_cancel_notification(appointment_info):
     messages = []
     for item in appointment_info:
         data = {
-                'appointment_id': item.id,
+                'id': f'{item.id}',
                 'message': f'your appointment of {item.visit_date} with doctor {item.doctor.name} has been cancelled',
             }
-        messages.append(Firebase.get_payload(data=data,reg_token=item.patient.notification_reg_token))
-    Firebase.send_batch_message(messages=messages)
+        message = Firebase.get_payload(data=data,
+                                       reg_token=item.patient.notification_reg_token)
+        messages.append(message)
+        Firebase.send_batch_message(messages=messages)
