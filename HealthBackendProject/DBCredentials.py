15be094e69ee1c93c7435ef557de9a60835827b8
Linkon Sid <linkon@sheba.xyz>
2021-11-21 15:59:26 +0600

    db credential removed from gitignore

---
+++ HealthBackendProject/DBCredentials.py
@@ -0,0 +1,8 @@
+class DBCredentials:
+    engine = 'django.db.backends.mysql'
+    name = 'eHealth'
+    user = 'root'
+    passwd = '01917079684'
+    host = 'localhost'
+    port = '3306'
+    #comment
